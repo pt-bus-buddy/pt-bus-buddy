a47cc9c418939ff46ff64c1072025e2c372318e6
darronese <darron198@hotmail.com>
2025-02-06 10:57:18 -0800

    added the rest of the pullman transit urls

--- server/index.js
+++ server/index.js
@@ -7,23 +7,39 @@ run().catch((err) => console.log(err));
 // function to get data
 async function run() {
   // variables
-  const url = "https://pullmanbusbeacon.com/gtfs-rt/vehiclepositions";
+  const positionUrl = "https://pullmanbusbeacon.com/gtfs-rt/vehiclepositions";
+  const realTimeUrl = "https://pullmanbusbeacon.com/gtfs-rt/tripupdates";
+  const alertsUrl = "https://pullmanbusbeacon.com/gtfs-rt/alerts";
 
-  // fetch data from url
+  // fetch responses
   // NEED to use await to make sure the promise is fulfilled
-  const response = await fetch(url);
+  // maybe we have to switch the position based on what we want faster?
+  const positionResponse = await fetch(positionUrl);
+  const realTimeResponse = await fetch(realTimeUrl);
+  const alertsResponse = await fetch(alertsUrl);
 
-  // convert into array buffer
-  const buffer = await response.arrayBuffer();
-  const uint8array = new Uint8Array(buffer);
+  // read the bodies
+  const positionBuffer = await positionResponse.arrayBuffer();
+  const realTimeBuffer = await realTimeResponse.arrayBuffer();
+  const alertsBuffer = await alertsResponse.arrayBuffer();
+
+  // convert into position buffers
+  const positionArray = new Uint8Array(positionBuffer);
+  const realTimeArray = new Uint8Array(realTimeBuffer);
+  const alertsArray = new Uint8Array(alertsBuffer);
 
   // load in the .proto file
   const root = await protobuf.load("./gtfs-realtime.proto");
-  //lookup feed message
-  const feedMessage = root.lookupType("transit_realtime.FeedMessage");
 
-  //decode the data into an object
-  const message = feedMessage.decode(uint8array);
+  // look up feed message from our schema
+  const message = root.lookupType("transit_realtime.FeedMessage");
 
-  console.log(message);
+  // decode protobuf sets
+  const positionMessage = message.decode(positionArray);
+  const realTimeMessage = message.decode(realTimeArray);
+  const alertsMessage = message.decode(alertsArray);
+
+  console.log("Position Message: ", positionMessage);
+  console.log("Real Time Message: ", realTimeMessage);
+  console.log("Alerts Messages: ", alertsMessage);
 }
