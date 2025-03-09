b0c1bfb2da42200ebd1ea4df6857615811da8775
darronese <darron198@hotmail.com>
2025-03-09 15:52:20 -0700

    API data should now be readable, we just have to figure out what data to use

--- server/index.js
+++ server/index.js
@@ -1,35 +1,6 @@
 import protobuf from "protobufjs";
 import fetch from "node-fetch";
 
-<<<<<<< HEAD
-// catch error
-run().catch((err) => console.log(err));
-
-// function to get data
-async function run() {
-  // variables
-  const positionUrl = "https://pullmanbusbeacon.com/gtfs-rt/vehiclepositions";
-  const realTimeUrl = "https://pullmanbusbeacon.com/gtfs-rt/tripupdates";
-  const alertsUrl = "https://pullmanbusbeacon.com/gtfs-rt/alerts";
-
-  // fetch responses
-  // NEED to use await to make sure the promise is fulfilled
-  // maybe we have to switch the position based on what we want faster?
-  const positionResponse = await fetch(positionUrl);
-  const realTimeResponse = await fetch(realTimeUrl);
-  const alertsResponse = await fetch(alertsUrl);
-
-  // read the bodies
-  const positionBuffer = await positionResponse.arrayBuffer();
-  const realTimeBuffer = await realTimeResponse.arrayBuffer();
-  const alertsBuffer = await alertsResponse.arrayBuffer();
-
-  // convert into position buffers
-  const positionArray = new Uint8Array(positionBuffer);
-  const realTimeArray = new Uint8Array(realTimeBuffer);
-  const alertsArray = new Uint8Array(alertsBuffer);
-
-=======
 // used to inspect objects and return string/data we can use
 // https://www.w3schools.com/nodejs/ref_util.asp
 import util from "util";
@@ -61,7 +32,6 @@ async function run() {
   const realTimeArray = new Uint8Array(realTimeBuffer);
   const alertsArray = new Uint8Array(alertsBuffer);
 
->>>>>>> darron
   // load in the .proto file
   const root = await protobuf.load("./gtfs-realtime.proto");
 
@@ -73,16 +43,10 @@ async function run() {
   const realTimeMessage = message.decode(realTimeArray);
   const alertsMessage = message.decode(alertsArray);
 
-<<<<<<< HEAD
-  console.log("Position Message: ", positionMessage);
-  console.log("Real Time Message: ", realTimeMessage);
-  console.log("Alerts Messages: ", alertsMessage);
-=======
   console.log("Position Message: ");
   console.log(util.inspect(positionMessage, { depth: null, colors: true }));
   console.log("Real Time Message: ", realTimeMessage);
   console.log(util.inspect(realTimeMessage, { depth: null, colors: true }));
   console.log("Alerts Messages: ", alertsMessage);
   console.log(util.inspect(alertsMessage, { depth: null, colors: true }));
->>>>>>> darron
 }
