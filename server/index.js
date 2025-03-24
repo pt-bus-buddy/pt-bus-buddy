88f9fa092a610e0d289c7b89b747d127d280b485
darronese <darron198@hotmail.com>
2025-03-24 14:27:43 -0700

    Fixed issue of non updating bus

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
-import { fetchAllDatasets } from "./fetchData.js";
 import { connectData } from "./connectData.js";
 // catch error
 run().catch((err) => console.log(err));
 
 // function to get data
 async function run() {
-  // fetch the data sets
-  const { positions, realTime, alerts } = await fetchAllDatasets();
-
   // connect it with web sockets
-  connectData(positions);
+  connectData();
 }
