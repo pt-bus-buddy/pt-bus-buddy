1c2920629736072496461925cf21bb80a074c833
darronese <darron198@hotmail.com>
2025-03-30 12:48:22 -0700

    added stop times id to connect stop id with bus id

--- server/calculate.js
+++ server/calculate.js
@@ -6,12 +6,12 @@ import { parse } from "csv-parse/sync";
 import fetch from "node-fetch";
 
 export async function processRaw(busPositions) {
-  // grab data from gtfs
-  const stopsUrl = "https://pullmanbusbeacon.com/gtfs";
-  const stopsResponse = await fetch(stopsUrl);
-  const stopsBuffer = await stopsResponse.arrayBuffer();
+  // grab stops data from gtfs
+  const staticUrl = "https://pullmanbusbeacon.com/gtfs";
+  const staticResponse = await fetch(staticUrl);
+  const staticBuffer = await staticResponse.arrayBuffer();
   // read stops.txt from zip
-  const zip = new AdmZip(Buffer.from(stopsBuffer));
+  const zip = new AdmZip(Buffer.from(staticBuffer));
   const stopsTxt = zip.readAsText("stops.txt");
   // parse stops.txt as CSV
   const stopsRecords = parse(stopsTxt, {
@@ -19,6 +19,23 @@ export async function processRaw(busPositions) {
     skip_empty_lines: true,
   });
 
+  // read trip.txt from zip
+  const tripsTxt = zip.readAsText("trips.txt");
+  // parse as CSV
+  const tripsRecords = parse(tripsTxt, {
+    columns: true,
+    skip_empty_lines: false,
+  });
+
+  // read stop_time.txt from zip
+  const timeTxt = zip.readAsText("stop_times.txt");
+  // parse as csv
+  const timeRecords = parse(timeTxt, {
+    columns: true,
+    skip_empty_lines: false,
+  });
+
+  /*
   // debugging step: printing all stops
   console.log("List of Stops:");
   stopsRecords.forEach((stop, index) => {
@@ -26,6 +43,25 @@ export async function processRaw(busPositions) {
       `#${index + 1}: [${stop.stop_id}] ${stop.stop_name} (${stop.stop_lat}, ${stop.stop_lon})`,
     );
   });
+  */
+
+  /*
+  // debugging step: printing all trips
+  console.log("List of Trips:");
+  tripsRecords.forEach((trip, index) => {
+    console.log(`#${index + 1}: [${trip.trip_id}]`);
+  });
+  */
+
+  /*
+  // debugging step: printing all the stop times
+  console.log("List of Stop Time:");
+  timeRecords.forEach((entry, index) => {
+    console.log(`${index + 1}: [${entry.trip_id}] Stop: ${entry.stop_id}`);
+  });
+  */
+
+  /*
   // debugging step: printing all bus positions and id
   console.log("\nLive Bus Positions:");
   busPositions.forEach((bus, index) => {
@@ -36,4 +72,5 @@ export async function processRaw(busPositions) {
     console.log("  Bearing:  ", bus.position.bearing);
     console.log("  Speed:    ", bus.position.speed);
   });
+  */
 }
