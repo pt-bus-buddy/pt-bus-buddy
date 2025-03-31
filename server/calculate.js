c935fa70de4c737f1c6ec78acc80358d38410d9e
darronese <darron198@hotmail.com>
2025-03-31 11:21:20 -0700

    Refactoring: made fetch static data it's own function, implementing eta with google maps api calculations

--- server/calculate.js
+++ server/calculate.js
@@ -1,66 +1,31 @@
-// parse csv
-import AdmZip from "adm-zip";
-import { parse } from "csv-parse/sync";
-
-// get url for stops
-import fetch from "node-fetch";
+import { fetchStatic } from "./staticData.js";
 
 export async function processRaw(busPositions) {
-  // grab stops data from gtfs
-  const staticUrl = "https://pullmanbusbeacon.com/gtfs";
-  const staticResponse = await fetch(staticUrl);
-  const staticBuffer = await staticResponse.arrayBuffer();
-  // read stops.txt from zip
-  const zip = new AdmZip(Buffer.from(staticBuffer));
-  const stopsTxt = zip.readAsText("stops.txt");
-  // parse stops.txt as CSV
-  const stopsRecords = parse(stopsTxt, {
-    columns: true,
-    skip_empty_lines: true,
-  });
-
-  // read trip.txt from zip
-  const tripsTxt = zip.readAsText("trips.txt");
-  // parse as CSV
-  const tripsRecords = parse(tripsTxt, {
-    columns: true,
-    skip_empty_lines: false,
-  });
-
-  // read stop_time.txt from zip
-  const timeTxt = zip.readAsText("stop_times.txt");
-  // parse as csv
-  const timeRecords = parse(timeTxt, {
-    columns: true,
-    skip_empty_lines: false,
-  });
-
+  // get our static data
+  var staticData = await fetchStatic();
   /*
   // debugging step: printing all stops
   console.log("List of Stops:");
-  stopsRecords.forEach((stop, index) => {
+  staticData.stopsRecords.forEach((stop, index) => {
     console.log(
       `#${index + 1}: [${stop.stop_id}] ${stop.stop_name} (${stop.stop_lat}, ${stop.stop_lon})`,
     );
   });
   */
-
   /*
   // debugging step: printing all trips
   console.log("List of Trips:");
-  tripsRecords.forEach((trip, index) => {
+  staticData.tripsRecords.forEach((trip, index) => {
     console.log(`#${index + 1}: [${trip.trip_id}]`);
   });
   */
-
   /*
   // debugging step: printing all the stop times
   console.log("List of Stop Time:");
-  timeRecords.forEach((entry, index) => {
+  staticData.timeRecords.forEach((entry, index) => {
     console.log(`${index + 1}: [${entry.trip_id}] Stop: ${entry.stop_id}`);
   });
   */
-
   /*
   // debugging step: printing all bus positions and id
   console.log("\nLive Bus Positions:");
